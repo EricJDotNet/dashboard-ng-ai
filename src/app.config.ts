b988720bc4f51960cb6b7b2058f351c40259242c
EricJDotNet <ericjones.net@gmail.com>
2025-12-18 11:25:35 -0500

    Auth0 redirect auth.

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,6 +3,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from './environments/environment';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 
@@ -11,6 +13,14 @@ export const appConfig: ApplicationConfig = {
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
         provideHttpClient(withFetch()),
         provideAnimationsAsync(),
-        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+        provideAuth0({
+            domain: environment.auth0.domain,
+            clientId: environment.auth0.clientId,
+            authorizationParams: {
+                redirect_uri: window.location.origin,
+                audience: environment.auth0.audience,
+            }
+        })
     ]
 };
